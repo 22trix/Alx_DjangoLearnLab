aa75561ab0074614f80299922e60d9926c1c5fa4
Goodness <goodnessojonuba@gmail.com>
2025-11-09 13:49:46 +0100

    views import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,17 +4,22 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseForbidden
 
+
 def list_books(request):
-	books = Book.objects.all()
-	return render(request, "relationship_app/list_books.html", {'books': books})
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {'books': books})
+
 
 class LibraryDetailView(DetailView):
-	model = Library
-	template_name = 'relationship_app/library_detail.html'
-	context_object_name = 'library'
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -26,35 +31,44 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
+
 class CustomLoginView(LoginView):
     template_name = 'relationship_app/login.html'
 
+
 class CustomLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
-	
+
+
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
 
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
 
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
 
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
+
 @login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+
 @login_required
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
@@ -68,6 +82,7 @@ def add_book(request):
         return redirect('list_books')
     return render(request, 'relationship_app/add_book.html')
 
+
 @permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
@@ -79,6 +94,7 @@ def edit_book(request, book_id):
         book.save()
         return redirect('list_books')
     return render(request, 'relationship_app/edit_book.html', {'book': book})
+
 
 @permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, book_id):
