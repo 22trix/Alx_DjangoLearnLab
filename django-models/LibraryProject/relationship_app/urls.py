183c9f7f943b9f8cf71f3faac63a9451de7e61b8
Goodness <goodnessojonuba@gmail.com>
2025-11-09 01:45:26 +0100

    url edits

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-
-app_name = 'relationship_app'
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import LogoutView
+from .views import LoginView
+from .views import LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
-    # Function-based view URL
-    path('books/', views.list_books, name='list_books'),
-    
-    # Class-based view URL
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-    path('', views.relationship_home, name='relationship_home'),  # Home page
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
+    path('', views.list_books, name='book_list_root'),
+    path('books/', list_books, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register',)
 ]
