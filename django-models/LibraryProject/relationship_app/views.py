ad85c799880740be9de402e23b5c75570a44fdb8
Goodness <goodnessojonuba@gmail.com>
2025-11-09 01:35:06 +0100

    view fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,53 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
 
-# Function-based view to list all books
+
 def list_books(request):
-    """Function-based view to display all books"""
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    """Lists all books using a function-based view."""
+    # Retrieve all Book objects
+    all_books = Book.objects.all().select_related('author')
 
-# Class-based view to display library details
+    context = {
+        'books': all_books,
+        'view_type': 'Function-Based View'
+    }
+    return render(request, 'relationship_app/list_books.html', context)
+
+
 class LibraryDetailView(DetailView):
-    """Class-based view to display library details"""
+    """Displays details for a specific library using a DetailView CBV."""
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        """Adds custom context data to the template."""
+        context = super().get_context_data(**kwargs)
+        context['view_type'] = 'Class-Based View'
+        return context
+
+
+class LoginView(LoginView):
+    template_name = 'registration/login.html'
+
+
+class LogoutView(LogoutView):
+    template_name = 'registration/logged_out.html'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+            context = {'form': form}
+            return render(request, 'relationship_app/register.html', context)
