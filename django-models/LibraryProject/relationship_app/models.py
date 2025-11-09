78226e229159931eaed47e34525eb73c5a508f53
Goodness <goodnessojonuba@gmail.com>
2025-11-09 13:46:10 +0100

    import fix

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 class Author(models.Model):
@@ -7,6 +9,7 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -14,20 +17,22 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
-    
+
     class Meta:
-        permissions = [
+        permissions = (
             ("can_add_book", "Can add a book"),
             ("can_change_book", "Can change a book"),
             ("can_delete_book", "Can delete a book"),
-        ]
+        )
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='library')
+    books = models.ManyToManyField(Book, related_name='libraries')
     
     def __str__(self):
         return self.name
+
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
@@ -36,22 +41,22 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+
 ROLE_CHOICES = (
     ('Admin', 'Admin'),
     ('Librarian', 'Librarian'),
     ('Member', 'Member'),
 )
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django's built-in User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f'{self.user.username} ({self.role})'
 
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add a book"),
-            ("can_change_book", "Can change a book"),
-            ("can_delete_book", "Can delete a book"),
-        ]
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='Member')
